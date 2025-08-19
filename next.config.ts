80c8964a97696ac9174719f51cb6acfe29a57547
samfeldman824 <samuel_feldman@brown.edu>
2025-08-19 13:47:49 -0400

    another config try

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
@@ -10,6 +12,29 @@ const nextConfig: NextConfig = {
       config.optimization = {
         ...config.optimization,
         minimize: true,
+        splitChunks: {
+          chunks: 'all',
+          cacheGroups: {
+            default: false,
+            vendors: false,
+            // Vendor chunk
+            vendor: {
+              name: 'vendor',
+              chunks: 'all',
+              test: /node_modules/,
+              priority: 20,
+            },
+            // Common chunk
+            common: {
+              name: 'common',
+              minChunks: 2,
+              chunks: 'all',
+              priority: 10,
+              reuseExistingChunk: true,
+              enforce: true,
+            },
+          },
+        },
       };
     }
     return config;
